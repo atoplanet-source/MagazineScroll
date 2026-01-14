346fc86af8f5e50b40181e6459966d359ef1c53b
August Orser <atoplanet@gmail.com>
2026-01-13 22:40:58 -0800

    Initial Commit

---
+++ MagazineScroll/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MagazineScroll
+//
+//  Created by August Orser on 1/13/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
